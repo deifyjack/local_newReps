1964bdc462ca1c0b0b96e319ce53d4a74b80ec97
deifyjack <deifyjack@hotmail.com>
2013-06-09 16:31:21 +0800

    测试项目

---
+++ TestProject/src/StringTest.java
@@ -0,0 +1,22 @@
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+
+public class StringTest {
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String ss = "ddtovvto";
+
+		System.out.println(ss.substring(0,ss.lastIndexOf("to")));
+
+	}
+
+}
